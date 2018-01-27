5f3edcaaa044a4b664c570faa7596a7c75593568
Lukas Bjarre <lbjarre@kth.se>
2018-01-27 12:15:50 +0100

    Added shuffling of generated dataset

--- scripts/datageneration.py
+++ scripts/datageneration.py
@@ -12,23 +12,36 @@ import matplotlib.pyplot as plt
 def lin_sep(size):
     """Generates two classes of linearly separable data"""
 
-    size_tup = (2, size)
-
     class1_mean = [1, 1]
     class1_cov = np.diag([0.1, 0.2])
+    class1_labels = np.ones((size, 1))
 
-    class1_x, class1_y = rnd.multivariate_normal(class1_mean, class1_cov, size_tup)
+    class1_xy = rnd.multivariate_normal(class1_mean, class1_cov, size)
+    class1_xy_labels = np.hstack((class1_xy, class1_labels))
 
     class2_mean = [-1, -1]
     class2_cov = np.diag([0.2, 0.1])
+    class2_labels = -1 * np.ones((size, 1))
 
-    class2_x, class2_y = rnd.multivariate_normal(class2_mean, class2_cov, size_tup)
+    class2_xy = rnd.multivariate_normal(class2_mean, class2_cov, size)
+    class2_xy_labels = np.hstack((class2_xy, class2_labels))
 
-    return class1_x, class1_y, class2_x, class2_y
+    xy_labels = np.vstack((class1_xy_labels, class2_xy_labels))
+    rnd.shuffle(xy_labels)
+    xy = xy_labels[:, 0:2]
+    labels = xy_labels[:, -1]
+
+    return xy, labels
 
 
 if __name__ == '__main__':
-    class1_x, class1_y, class2_x, class2_y = lin_sep(100)
-    plt.scatter(class1_x, class1_y, color='r')
-    plt.scatter(class2_x, class2_y, color='b')
+    data, labels = lin_sep(100)
+
+    label_color_map = {
+        1: 'b',
+        -1: 'r'
+    }
+    label_colors = list(map(lambda x: label_color_map.get(x), labels))
+
+    plt.scatter(data[:, 0], data[:, 1], c=label_colors)
     plt.show()
