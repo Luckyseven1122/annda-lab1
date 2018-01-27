e11d7d4ae2da0af297aa5e8b9fa1c4b5c5e88ab3
Lukas Bjarre <lbjarre@kth.se>
2018-01-27 12:28:12 +0100

    Fixed vector shape in data generation

--- scripts/datageneration.py
+++ scripts/datageneration.py
@@ -14,22 +14,22 @@ def lin_sep(size):
 
     class1_mean = [1, 1]
     class1_cov = np.diag([0.1, 0.2])
-    class1_labels = np.ones((size, 1))
+    class1_labels = np.ones(size)
 
-    class1_xy = rnd.multivariate_normal(class1_mean, class1_cov, size)
-    class1_xy_labels = np.hstack((class1_xy, class1_labels))
+    class1_xy = rnd.multivariate_normal(class1_mean, class1_cov, size).T
+    class1_xy_labels = np.vstack((class1_xy, class1_labels))
 
     class2_mean = [-1, -1]
     class2_cov = np.diag([0.2, 0.1])
-    class2_labels = -1 * np.ones((size, 1))
+    class2_labels = -1 * np.ones(size)
 
-    class2_xy = rnd.multivariate_normal(class2_mean, class2_cov, size)
-    class2_xy_labels = np.hstack((class2_xy, class2_labels))
+    class2_xy = rnd.multivariate_normal(class2_mean, class2_cov, size).T
+    class2_xy_labels = np.vstack((class2_xy, class2_labels))
 
-    xy_labels = np.vstack((class1_xy_labels, class2_xy_labels))
+    xy_labels = np.hstack((class1_xy_labels, class2_xy_labels))
     rnd.shuffle(xy_labels)
-    xy = xy_labels[:, 0:2]
-    labels = xy_labels[:, -1]
+    xy = xy_labels[0:2, :]
+    labels = xy_labels[-1, :]
 
     return xy, labels
 
@@ -43,5 +43,5 @@ if __name__ == '__main__':
     }
     label_colors = list(map(lambda x: label_color_map.get(x), labels))
 
-    plt.scatter(data[:, 0], data[:, 1], c=label_colors)
+    plt.scatter(data[0, :], data[1, :], c=label_colors)
     plt.show()
