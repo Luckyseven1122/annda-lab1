1e7eedfa30d34709161d39350b95a7fe12bd5621
Lukas Bjarre <lbjarre@kth.se>
2018-01-27 13:41:05 +0100

    Fixed incorrect shuffling with column vectors

--- scripts/datageneration.py
+++ scripts/datageneration.py
@@ -12,14 +12,14 @@ import matplotlib.pyplot as plt
 def lin_sep(size):
     """Generates two classes of linearly separable data"""
 
-    class1_mean = [1, 1]
+    class1_mean = [0, 0]
     class1_cov = np.diag([0.1, 0.2])
     class1_labels = np.ones(size)
 
     class1_xy = rnd.multivariate_normal(class1_mean, class1_cov, size).T
     class1_xy_labels = np.vstack((class1_xy, class1_labels))
 
-    class2_mean = [-1, -1]
+    class2_mean = [-1.5, -1.5]
     class2_cov = np.diag([0.2, 0.1])
     class2_labels = -1 * np.ones(size)
 
@@ -27,7 +27,7 @@ def lin_sep(size):
     class2_xy_labels = np.vstack((class2_xy, class2_labels))
 
     xy_labels = np.hstack((class1_xy_labels, class2_xy_labels))
-    rnd.shuffle(xy_labels)
+    rnd.shuffle(xy_labels.T)
     xy = xy_labels[0:2, :]
     labels = xy_labels[-1, :]
 
